f5e847c58f07e439fa56db8304f6a67b99fb3e2a
Geoff Williams <geoff@geoffwilliams.me.uk>
2015-11-08 02:16:32 +1100

    cleanup prompts

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,10 +3,10 @@ class bash_user_skel::params {
   # prompt colouring borrowed from https://wiki.archlinux.org/index.php/Color_Bash_Prompt
 
   # colourful prompt for normal users
-  $ps1 = "\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]"
+  $ps1 = "\[\e[0;32m\]\u@\h:\[\e[m\]\[\e[1;34m\]\w\[\e[m\]\$ "
 
   # red prompt for root
-  $ps1_root = "\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[0;31m\]\# \[\e[m\]\[\e[0;32m\]"
+  $ps1_root = "\[\e[0;31m\]\u@\h:\[\e[m\]\[\e[1;34m\]\w\[\e[m\]\[\e[0;31m\]# \[\e[m\]\[\e[0;32m\]"
 
   $aliases = [
     "ls='ls --color=auto -shF'",
